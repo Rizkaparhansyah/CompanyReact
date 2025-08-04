ba9282ae513c8e90f25ab2efef33a38edb0cddab
Rizkaparhansyah <rizka.parhan@gmail.com>
2025-08-04 21:57:52 +0700

    final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
             </li>
             <li>
               <Link to="category" spy={true} smooth={true}>
-                Category
+                Zakat
               </Link>
             </li>
             <li>
